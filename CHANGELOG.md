8519d699400d9fc3be768ab8c549655451b42ed6
Graciliano M. Passos <gracilianomp@gmail.com>
2018-09-25 17:32:02 -0300

    Fix swiss_knife_browser.dart exports.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+
+## 1.0.1
+
+- Fix swiss_knife_browser.dart exports.
+
 ## 1.0.0
 
 - Added events example.
--- lib/swiss_knife_browser.dart
+++ lib/swiss_knife_browser.dart
@@ -1,4 +1,7 @@
 library swiss_knife;
 
+export 'src/utils.dart';
 export 'src/utils_browser.dart';
+export 'src/locales.dart';
 export 'src/locales_browser.dart';
+export 'src/rest_client.dart';
