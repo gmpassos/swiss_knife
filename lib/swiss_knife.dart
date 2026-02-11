7ca6a553637d345429b7407f3206a65e4ce933c8
gmpassos <gracilianomp@gmail.com>
2026-02-11 06:12:42 -0300

    ✨ feat(lib/swiss_knife.dart): export src/flat_hash_map.dart to package public API

--- lib/swiss_knife.dart
+++ lib/swiss_knife.dart
@@ -4,6 +4,7 @@ export 'src/collections.dart';
 export 'src/data.dart';
 export 'src/date.dart';
 export 'src/events.dart';
+export 'src/flat_hash_map.dart';
 export 'src/json.dart';
 export 'src/loader.dart';
 export 'src/math.dart';
